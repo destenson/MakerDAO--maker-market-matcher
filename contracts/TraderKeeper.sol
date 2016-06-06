1eb7c470991dc40333d2201c17b2253745172ded
Freek <freekpst@gmail.com>
2016-06-06 09:36:31 +0200

    Updated keeper contract to handle allowances and call the tokens directly

--- contracts/TraderKeeper.sol
+++ contracts/TraderKeeper.sol
@@ -1,40 +1,43 @@
 import 'maker-otc/simple_market.sol';
 import 'maker-otc/assertive.sol';
+import 'erc20/erc20.sol';
 
 contract TraderKeeper is Assertive {
     
-    struct Wallet {
-        mapping (bytes32 => uint) tokens;
+    SimpleMarket maker_market;
+    address maker_market_address
+    address owner;
+    
+    function TraderKeeper (SimpleMarket market_contract, address maker_address) {
+        maker_market = market_contract; 
+        maker_market_address = maker_address;
+        owner = msg.sender;
     }
     
-    mapping(address => Wallet) users;
-    SimpleMarket maker_address;
-    
-    function TraderKeeper (SimpleMarket contract_address) {
-        maker_address = contract_address;
-    }
-    
-    function withdraw(bytes32 which_token, uint token_amount) {
-        var user = users[msg.sender];
-        if(user.tokens[which_token] >= token_amount) {
-            //transfer(msg.sender, token_amount, which_token);
-            user.tokens[which_token] -= token_amount;
-        }
-        else {
-            //transfer(msg.sender, user.tokens[which_token], which_token);
-            user.tokens[which_token] = 0;
+    function withdraw(ERC20 token, uint token_amount) {
+        if(msg.sender == owner) {
+            token.transfer(msg.sender, token_amount);
         }
     }
     
-    function deposit(bytes32 which_token, uint token_amount) {
-        //transferFrom(msg.sender, token_amount, which_token);
-        users[msg.sender].tokens[which_token] += token_amount;
+    function deposit(ERC20 token, uint token_amount) {
+        if(msg.sender == owner) {
+            token.transferFrom(msg.sender, this, token_amount);
+        }
     }
     
     //initially only the amount that was bought can be sold, so quantity is the same for bid/ask
-    function trade(uint bid_id, uint ask_id, uint quantity) {
-        //maker_address.buyPartial(bid_id, quantity);
-        //maker_address.buyPartial(ask_id, quantity);
+    function trade(uint bid_id, uint ask_id, uint quantity, ERC20 buying, ERC20 selling) {
+        var buy_allowance = buying.allowance(this, maker_market_address);
+        if(buy_allowance < 500) {
+            buying.approve(maker_market_address, 1000);
+        }
+        var sell_allowance = selling.allowance(this, maker_market_address);
+        if(sell_allowance < 500) {
+            selling.approve = selling.approve(maker_market_address, 1000);
+        }
+        maker_market.buyPartial(bid_id, quantity);
+        maker_market.buyPartial(ask_id, quantity);
         //assert(bidSuccess);
         //assert(askSuccess);
     }

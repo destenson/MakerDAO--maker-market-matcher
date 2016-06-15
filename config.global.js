715ab789bce1e16c36a7903df4c9699b91ced0ac
Freek <freekpst@gmail.com>
2016-06-15 15:53:12 +0200

    Added config file

---
+++ config.global.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.ERC20 = {};
+
+config.ERC20.ETH =  '0x52fe88b987c7829e5d5a61c98f67c9c14e6a7a90';
+config.ERC20.MKR = '0xffb1c99b389ba527a9194b1606b3565a07da3eef';
+config.trade_gas_costs = 0;
+
+module.exports = config;
